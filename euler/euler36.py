232e861c481d034cec58ccec167a50612a1793bc
Jay Engineer <jayengineer6@gmail.com>
2013-12-03 18:55:25 -0500

    a better palidrome

--- euler/euler36.py
+++ euler/euler36.py
@@ -10,28 +10,9 @@ Find the sum of all numbers, less than one million, which are palindromic in bas
 
 """
 
-def first(word):
-    """Returns the first character of a string."""
-    return word[0]
-
-
-def last(word):
-    """Returns all but the first character of a string."""
-    return word[-1]
-
-
-def middle(word):
-    """Returns all but the first and last characters of a string."""
-    return word[1:-1]
-
-
-def is_palindrome(word):
-    """Returns True if word is a palindrome."""
-    if len(word) <= 1:
+def is_palindrome(n):
+    if str(n) == str(n)[::-1]:
         return True
-    if first(word) != last(word):
-        return False
-    return is_palindrome(middle(word))
 
 def check_base():
 	numbers = []

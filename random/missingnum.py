bcd1d122a5478d9ca1d77e90abfe2057935f5bf1
Jay Engineer <jayengineer6@gmail.com>
2013-12-03 04:14:24 -0500

    added some more problems

---
+++ random/missingnum.py
@@ -0,0 +1,11 @@
+'''
+
+Missing Number from a List
+
+An array from 1 to N and a number is missing find out 3 ways to find this Specific number.
+
+1. Use a fast sorting algorithm (radix sort or even quicksort which is O(N log N) and iterate through that.
+2. Add all the numbers from 1 to N and than subtract the sum from the actual sum of all the numbers from 1 to N and the missing number that is subtracted is your number.
+3. ?
+
+'''
---
+++ random/repeatword.py
@@ -0,0 +1,5 @@
+'''
+
+Repeat Number
+
+'''

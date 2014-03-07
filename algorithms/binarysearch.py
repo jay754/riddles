a30382520b90635d302e04dd003255185a7284bc
Jay Engineer <jayengineer6@gmail.com>
2014-03-07 14:38:44 -0500

    whitespace

--- algorithms/binarysearch.py
+++ algorithms/binarysearch.py
@@ -1,22 +1,24 @@
 """
+
 http://en.wikipedia.org/wiki/Binary_search_algorithm
 
 Finds the position of a specific element within an array
+
 """
 
 def binarysearch(a, value):
-	low = 0
-	high = len(a) - 1
+    low = 0
+    high = len(a) - 1
 
-	while(low <= high):
+    while(low <= high):
 
-		mid = (low + high)/2
+        mid = (low + high)/2
 
-		if a[mid] > value:
-			high = mid - 1
-		else:
-			low = mid + 1
+        if a[mid] > value:
+            high = mid - 1
+        else:
+            low = mid + 1
 
-	return low
+    return low
 
 print binarysearch([1, 3, 4, 8, 6], 6)

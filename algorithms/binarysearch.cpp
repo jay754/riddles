e01b8a999865ef21bd46fb11b16a7d5514a41480
Jay Engineer <jayengineer6@gmail.com>
2014-03-17 00:55:27 -0400

    added bs

--- algorithms/binarysearch.cpp
+++ algorithms/binarysearch.cpp
@@ -6,28 +6,28 @@ int binarysearch(int a[], int size, int value);
 
 int binarysearch(int a[], int size, int value){
 
-	int low = 0;
-	int high = size - 1;
+    int low = 0;
+    int high = size - 1;
 
-	while (low <= high){
+    while (low <= high){
 
-		int mid = (low + high) / 2;
+        int mid = (low + high) / 2;
 
-		if(a[mid] > value){
-			high = mid - 1;
-		}
-		else if (a[mid] < value){
-			low = mid + 1;
-		}
-		else{
-			return mid;
-		}
-	}
+        if(a[mid] > value){
+            high = mid - 1;
+        }
+        else if (a[mid] < value){
+            low = mid + 1;
+        }
+        else{
+            return mid;
+        }
+    }
 
-	return -1;
+    return -1;
 }
 
 int main(){
-	int a[] = {1, 3, 4, 8, 6};
-	cout << binarysearch(a, 5 ,8) << endl;
+    int a[] = {1, 3, 4, 8, 6};
+    cout << binarysearch(a, 5 ,8) << endl;
 }

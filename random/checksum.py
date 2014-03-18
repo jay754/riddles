38c3a513e88999d062c4db2739728c326310dac4
Jay Engineer <jayengineer6@gmail.com>
2014-03-18 16:47:37 -0400

    trailing whitespace

--- random/checksum.py
+++ random/checksum.py
@@ -7,12 +7,11 @@ Fletcher-16
 """
 
 def checksum(data):
+    sum1 = 0
+    sum2 = 0
 
-	sum1 = 0
-	sum2 = 0
+    for i in data:
+        sum1 = (sum1 + ord(i)) % 255
+        sum2 = sum1 + sum2 % 255
 
-	for i in data:
-		sum1 = (sum1 + ord(i)) % 255
-		sum2 = sum1 + sum2 % 255
-
-	return (sum2 << 8) | sum1
+    return (sum2 << 8) | sum1
--- random/easy136.py
+++ random/easy136.py
@@ -9,9 +9,9 @@ fileObj = file("easy136.txt", "r")
 stats = []
 
 for i in fileObj.readlines():
-	line = i.split()
-	averages = round(sum(map(float, line[1:]))/len(line[1:]), 3)
-	stats.append([line[0], averages])
+    line = i.split()
+    averages = round(sum(map(float, line[1:]))/len(line[1:]), 3)
+    stats.append([line[0], averages])
 
 for i in range(len(stats)):
-	print "student Name: " + str(stats[i][0]) +  " , " + "student Average: " + str(stats[i][1])
+    print "student Name: " + str(stats[i][0]) +  " , " + "student Average: " + str(stats[i][1])

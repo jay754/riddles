3bd803d28248a772bc74d6e5a6ef925d14956b15
Jay Engineer <jayengineer6@gmail.com>
2015-08-26 15:08:05 -0400

    added a basic linkedlist

--- data-structures/linkedlist.py
+++ data-structures/linkedlist.py
@@ -4,36 +4,27 @@ class Node:
 		self.value = value
 		self.next = next
 
-class linkedlist:
+class Linkedlist:
 
-	def __init__(self):
-		self.head = None
+	def __init__(self, head=None):
+		self.head = head
 
 	def add_node(self, value):
-		if self.head == None:
-			self.head = Node(value)
-		else:
-			self.head.next = Node(value)
+		new_node = Node(value)
+		new_node.next = self.head
+		self.head = new_node
 
 	def delete_node(self, value):
-		#a bit broken
 		prev = None
 		found = False
 		current_node = self.head
 
-		while not found:
+		while found:
 			if current_node.value == value:
 				found = True
-				current_node.value = None
 			else:
-				prev = current_node.value
+				prev = current_node
 				current_node = current_node.next
-
-	def print_list(self):
-		current_node = self.head
-		while current_node != None:
-			print current_node.value
-			current_node = current_node.next
 
 	def length(self):
 		current_node = self.head
@@ -57,12 +48,16 @@ class linkedlist:
 				current_node = current_node.next
 				count += 1
 
-if __name__ == '__main__':
+#testing
 
-	test = linkedlist()
-	test.add_node(30)
-	test.add_node(45)
-	test.add_node(16)
-	test.print_list()
-	print test.length()
-	test.get_position(16)
+test = Linkedlist()
+test.add_node(30) #points to None which the value will equal to and end the while loop
+test.add_node(29) #points to 30
+test.add_node(25) #points to 29
+test.add_node(16) #points to 25
+
+print test.length()
+
+test.delete_node(29)
+
+print test.length()

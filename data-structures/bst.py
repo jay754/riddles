d596bdbc2c770885fbebfa32e5fb7769994de409
Jay Engineer <jayengineer6@gmail.com>
2015-08-31 01:54:08 -0400

    added upgraded bst

--- data-structures/bst.py
+++ data-structures/bst.py
@@ -1,3 +1,5 @@
+# https://dl.dropboxusercontent.com/u/6665854/binarytree.py
+
 class Node:
     def __init__(self, key):
         self.key = key
@@ -5,30 +7,34 @@ class Node:
         self.left = None
 
 class BST:
-    def __int__(self):
-        self.root = None
+    def __init__(self, root):
+        self.root = root
 
     def insert(self, node, key):
         if node == None:
-            return Node(key)
+            node = Node(key)
 
         if node.key > key:
-            self.insert(node.left, key)
+            if node.left == None:
+                node.left = Node(key)
+            else:
+                self.insert(node.left, key)
         elif node.key < key:
-            self.insert(node.right, key)
+            if node.right == None:
+                node.right = Node(key)
+            else:
+                self.insert(node.right, key)
 
     def search(self, node, key):
         if node.key == key:
             return True
 
         if node.key > key:
-            self.search(node.left, key)
+            return self.search(node.left, key)
         elif node.key < key:
-            self.search(node.right, key)
+            return self.search(node.right, key)
 
-        return None
-
-    def get_minimum(node):
+    def get_minimum(self, node):
         current_node = node
 
         while current_node.left != None:
@@ -36,10 +42,44 @@ class BST:
 
         return current_node.key
 
-    def get_maximum(node):
+    def get_maximum(self, node):
         current_node = node
 
         while current_node.right != None:
             current_node = current_node.right
 
         return current_node.key
+
+    def preOrder(self, node):
+        if node != None:
+            print node.key
+            self.preOrder(node.left)
+            self.preOrder(node.right)
+
+    def inOrder(self, node):
+        if node != None:
+            self.inOrder(node.left)
+            print node.key
+            self.inOrder(node.right)
+
+    def postOrder(self, node):
+        if node != None:
+            self.postOrder(node.left)
+            self.postOrder(node.right)
+            print node.key
+
+root = Node(5)
+test = BST(root)
+test.insert(root, 30)
+test.insert(root, 50)
+test.insert(root, 4)
+test.insert(root, 43)
+
+print test.get_maximum(root)
+print test.get_minimum(root)
+
+print test.search(root, 49)
+
+# test.preOrder(root)
+# test.inOrder(root)
+# test.postOrder(root)
